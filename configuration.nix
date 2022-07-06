e9af20d7f3a7b2035897c559a78b6e1c91cda80d
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-07-06 02:23:22 +0200

    add chrome-gnome-shell integration

--- configuration.nix
+++ configuration.nix
@@ -40,6 +40,8 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  # Enable chrome gnome shell integration
+  services.gnome.chrome-gnome-shell.enable
 
   # Configure keymap in X11
   services.xserver = {
